f98b22dbcbbb9bba9dc4da57776c6d70da8121e4
cwarmbrunn <cwarmbrunn97@gmail.com>
2022-06-10 10:27:20 -0400

    continuing to add to the idb.js file

--- public/js/idb.js
+++ public/js/idb.js
@@ -13,4 +13,30 @@ request.onupgradeneeded = function (event) {
   // Create an object store called 'budget'
   // Set it to have an auto-incrementing primary key
   db.createObjectStore("budget", { autoIncrement: true });
+
+  // Upon a successful connection
+  request.onsuccess = function (event) {
+    // When database is successfully created with its object store
+    db = event.target.result;
+
+    // Check if the application is online - if yes, then run the below function
+    // This will send all local database data to API
+
+    // TODO: This hasn't been created yet - but it will be soon, commenting out for now
+    // uploadBudget();
+  };
 };
+
+request.onerror = function (event) {
+  // Log the error here
+  console.log(event.target.errorCode);
+};
+
+/// This function will be executed if an attempt is made to submit and there's no internet connection
+function saveRecord(record) {
+  // Open a new transaction with the database with read and write permissions
+  const transaction = db.transaction(["new_budget"], "readwrite");
+
+  // Access the object store for "new_budget"
+  const 
+}
