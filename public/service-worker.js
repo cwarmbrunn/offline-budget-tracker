c99e4306a7f93012c3adb3352c8c6b5dc22ddc32
cwarmbrunn <cwarmbrunn97@gmail.com>
2022-06-10 11:25:37 -0400

    added in final touches to server-worker.js file

--- public/service-worker.js
+++ public/service-worker.js
@@ -62,3 +62,39 @@ self.addEventListener("activate", function (evt) {
 });
 
 // Intercept fetch requests
+self.addEventListener("fetch", function (evt) {
+  if (evt.request.url.includes("/api/")) {
+    evt.respondWith(
+      caches
+        .open(DATA_CACHE_NAME)
+        .then((cache) => {
+          return fetch(evt.request)
+            .then((response) => {
+              // If 200 response, then clone it and store it in the cache
+              if (response.status === 200) {
+                cache.put(evt.request.url, response.clone());
+              }
+              return response;
+            })
+            .catch((err) => {
+              // Network request failed - try to get it from the cache
+              return cache.match(evt.request);
+            });
+        })
+        .catch((err) => console.log(err))
+    );
+    return;
+  }
+  evt.respondWith(
+    fetch(evt.request).catch(function () {
+      return caches.match(evt.request).then(function (response) {
+        if (response) {
+          return response;
+        } else if (evt.request.headers.get("accept").includes("text/html")) {
+          // Return the cached home page for all requests for HTML pages
+          return caches.match("/");
+        }
+      });
+    })
+  );
+});
